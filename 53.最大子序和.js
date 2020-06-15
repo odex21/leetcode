35f2e82951ab7aab2eb8a87ca76299061c55f06f
wuzhiming <wuzhiming@tuzhanai.com>
2020-06-15 19:26:10 +0800

    动态规划

--- 53.最大子序和.js
+++ 53.最大子序和.js
@@ -10,27 +10,13 @@
  * @return {number}
  */
 var maxSubArray = function (nums) {
+  let pre = 0, maxAns = nums[0]
+  nums.forEach((x) => {
+    pre = Math.max(pre + x, x)
+    maxAns = Math.max(maxAns, pre)
+  })
+  return maxAns
 
-  const find = (nums) => {
-    if (nums.length === 1) {
-      const res = nums[0]
-      return [res, res, res, res]
-    }
-
-    const m = Math.floor(nums.length / 2)
-    const [leftLSum, leftMSum, leftRSum, leftISum] = find(nums.slice(0, m))
-    const [rightLSum, rightMSum, rightRSum, rightISum] = find(nums.slice(m, nums.length))
-
-    // 左边的左边，左边全部+右边的右边
-    const lSum = Math.max(leftLSum, leftISum + rightLSum)
-    const rSum = Math.max(rightRSum, rightISum + leftRSum)
-    const mSum = Math.max(rightMSum, leftMSum, leftRSum + rightLSum)
-    const iSum = leftISum + rightISum
-
-    return [lSum, mSum, rSum, iSum]
-  }
-
-  return Math.max(...find(nums))
 }
 // @lc code=end
 
