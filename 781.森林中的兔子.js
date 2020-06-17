41aecb584071379ad664100cd1722cc8beeb2794
wuzhiming <wuzhiming@tuzhanai.com>
2020-06-17 14:50:34 +0800

    稍微优化下

--- 781.森林中的兔子.js
+++ 781.森林中的兔子.js
@@ -13,6 +13,7 @@ var numRabbits = function (answers) {
   const colors = answers.reduce((colors, cur) => {
     const total = colors.get(cur)
     colors.set(cur, total ? total + 1 : 1)
+    return colors
   }, new Map())
 
   let res = 0
@@ -21,12 +22,11 @@ var numRabbits = function (answers) {
 
   colors.forEach((value, key) => {
     const rate = value > key + 1 ? value / (key + 1) : 1
-    res += rate > 1 ? Math.floor(rate) * (key + 1) + ((rate * 10 % 10) ? getLeast(key) : 0) : getLeast(key)
+    res += rate > 1 ? Math.floor(rate) * (key + 1) + ((rate % 1) ? getLeast(key) : 0)
+      : getLeast(key)
   })
   return res
 }
 // @lc code=end
 
-// const test = numRabbits([0, 0, 1, 1, 1])
-// console.log('res', test)
 
